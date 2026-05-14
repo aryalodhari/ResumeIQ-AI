598ec95563fcec49d0795e8ca6b96fa9701ccae4
Arya Lodhari <aryalodhari224@gmail.com>
2026-05-14 13:35:30 +0530

    Added login and dashboard logic

--- app.py
+++ app.py
@@ -6,11 +6,88 @@ import docx
 import json
 
 app = Flask(__name__)
+app.secret_key = "secret12345"
+
+Base.metadata.create_all(bind=engine)
 
 @app.route('/')
 def home():
-    return "App is running"
 
+    if "user" in session:
+        return redirect("/dashboard")
+    return redirect("/login")
+
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    db = SessionLocal
+
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        existing_user = db.query(models.User).filter_by(email=email).first()
+        if existing_user:
+            return "User already exists"
+
+        user = models.User(email=email, password=password)
+        db.add()
+        db.commit()
+
+        return redirect("/login")
+    return render_template("signup.html")
+
+@app.route('/login', methods=["GET","POST"])
+def login():
+    db = SessionLocal
+
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = db.query(models.User).filter_by(email=email).first()
+
+        if user:
+            session["user"] = user.email
+            return redirect("/dashboard")
+        else:
+            return "Invalid Credentials"
+    return render_template(login.html)
+
+@app.route('/dashboard',methods=["GET","POST"])
+def dashboard():
+    if "user" not in session:
+        return redirect("/login")
+    
+    result = None
+
+    if request.method == "POST":
+        user_goal = request.form.get("role")
+        resume_text = request.form.get("resume")
+
+        file = request.file.get("file")
+
+        #file handling
+        if file and file.name != "":
+            if file.filename.endswith(".pdf"):
+                try:
+                    pdf_reader = PyPDF2.PdfReader(file)
+                    text = ""
+                    for page in pdf_reader.pages:
+                        text += page.extract_text() or "" 
+                    resume_text = text  
+                except Exception as e:
+                    result = {"error": f"PDF error:{str(e)}"} 
+            
+            elif file.filename.endswith(".docx"):
+                try:
+                    doc = docx.Document(file)
+                    text = ""
+                    for para in doc.paragraphs:
+                        text += para.text +"\n"
+                    resume_text = text
+                except Exception as e:
+                    result = {"error:" f"Docx error:{str(e)}"}
+                    
 if __name__ == "__main__":
     app.run(debug=True)
 
